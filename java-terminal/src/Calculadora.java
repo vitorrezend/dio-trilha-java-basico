a8c86de0dc7709ec534b6cd8513af283209fd309
Vitor Rezende <vagrant@master>
2023-06-09 19:04:58 +0000

    comentarios

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Vitor Rezende
+ * @version 1.0
+ * @since 09/06/2023
+ * 
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm Este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+        
+}
