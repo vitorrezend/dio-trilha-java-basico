e5362333b8659c75b84bcc3071069219c2c7d9ae
Vitor Rezende <vagrant@master>
2023-06-09 13:13:32 +0000

    Operadores 2

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,18 +3,22 @@ public class Operadores {
     public static void main(String[] args) {
         
         String concatenacao = "?";
+        System.out.println(concatenacao);
 
         concatenacao = 1+1+1+"1";
-
         System.out.println(concatenacao);
 
         concatenacao = 1 + "1" +1+1;
+        System.out.println(concatenacao);
 
         concatenacao = 1+"1"+1+"1";
-        
+        System.out.println(concatenacao);
+
         concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
 
         concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
 
     }
 }
