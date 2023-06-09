6dff5db620541c1dce2805e376a06a85d43ebd3f
Vitor Rezende <vagrant@master>
2023-06-09 12:52:01 +0000

    tiposvariaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo=2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final int numero = 1;
+
+        System.out.println(numero);
+
+    }
+}
