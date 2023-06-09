d60a64ff04043e8fa1660c4c7093916af0d58aef
Vitor Rezende <vagrant@master>
2023-06-09 15:19:26 +0000

    Metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.out.println("Volume Atual ? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.out.println("Volume Atual ? " + smartTv.volume);
+
+        
+
+        smartTv.desligar();
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+    }
+}
